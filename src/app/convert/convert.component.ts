1f7430e434675b6b6a7d86681d264cce920b7496
pedroTrovs <pedrotrovilho@gmail.com>
2023-04-11 09:49:45 -0300

    v4

--- src/app/convert/convert.component.ts
+++ src/app/convert/convert.component.ts
@@ -6,8 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./convert.component.css']
 })
 export class ConvertComponent {
-  moeda1: string = "";
-  moeda2: string = "";
+  moeda1: string = "Moeda de entrada";
+  moeda2: string = "Moeda de saída";
   value: number = 0;
   ratio: number = 0;
   calc()
@@ -15,16 +15,19 @@ export class ConvertComponent {
     try
     {
       let r;
+
+      if(this.moeda1 == "Moeda de entrada" || this.moeda2 == "Moeda de saída")
+        throw("Selecione as moedas!");
+
       if(this.moeda1 == this.moeda2)
         throw("Não é possível converter uma moeda em si mesma!");
-      else
-      {
-        r = this.value * this.ratio;
-      }
+
+      r = this.value * this.ratio;
+      alert(this.moeda1 + "(" + this.value + ")" + " -> " + this.moeda2 + "(" + r.toFixed(2) +")");
     }
     catch(e)
     {
-      alert("Erro: " + e);
+      alert("Erro -> " + e);
     }
   }
 }
