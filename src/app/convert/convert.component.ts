ab5c0f142a0967fa52b8bbb34862ab72b2c70cc9
pedroTrovs <pedrotrovilho@gmail.com>
2023-04-11 09:30:33 -0300

    v3

--- src/app/convert/convert.component.ts
+++ src/app/convert/convert.component.ts
@@ -6,5 +6,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./convert.component.css']
 })
 export class ConvertComponent {
-
+  moeda1: string = "";
+  moeda2: string = "";
+  value: number = 0;
+  ratio: number = 0;
+  calc()
+  {
+    try
+    {
+      let r;
+      if(this.moeda1 == this.moeda2)
+        throw("Não é possível converter uma moeda em si mesma!");
+      else
+      {
+        r = this.value * this.ratio;
+      }
+    }
+    catch(e)
+    {
+      alert("Erro: " + e);
+    }
+  }
 }
